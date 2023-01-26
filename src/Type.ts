cfd1a86c5e90d04c0561003fd36a6568fb75048c
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2023-01-26 17:53:49 +0800

    - 菜单解耦

---
+++ src/Type.ts
@@ -0,0 +1,6 @@
+export interface ListBarData {
+  icon: JSX.Element;
+  label: string;
+  link: string;
+  children?: ListBarData[];
+}
--- src/pages/dashboard/listItems.tsx
+++ src/pages/dashboard/listItems.tsx
@@ -2,84 +2,109 @@ import * as React from "react";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import ListSubheader from "@mui/material/ListSubheader";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import PeopleIcon from "@mui/icons-material/People";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import LayersIcon from "@mui/icons-material/Layers";
-import AssignmentIcon from "@mui/icons-material/Assignment";
-import { Divider, List } from "@mui/material";
+import { Divider, List, Collapse } from "@mui/material";
 import { Link as Rlink } from "react-router-dom";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+
 import UserAvatar from "../../components/UserAvatar";
 import UserMenu from "./UserMenu";
-
-import Box from "@mui/material/Box";
-import { styled, ThemeProvider, createTheme } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import ArrowRight from "@mui/icons-material/ArrowRight";
-import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
-import Home from "@mui/icons-material/Home";
-import Settings from "@mui/icons-material/Settings";
 import People from "@mui/icons-material/People";
 import PermMedia from "@mui/icons-material/PermMedia";
 import Dns from "@mui/icons-material/Dns";
 import Public from "@mui/icons-material/Public";
-import ListItem from "@mui/material/ListItem";
+import { ListBarData } from "../../Type";
 
-export const mainListItems = (
-  <React.Fragment>
-    <ListItemButton component={Rlink} to="home">
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItemButton>
-    <ListItemButton component={Rlink} to="test">
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="订单" />
-    </ListItemButton>
-    <ListItemButton component={Rlink} to="/dashboard/test/hello">
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Customers" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Reports" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Integrations" />
-    </ListItemButton>
-  </React.Fragment>
-);
-const data = [
-  { icon: <People />, label: "Authentication" },
-  { icon: <Dns />, label: "Database" },
-  { icon: <PermMedia />, label: "Storage" },
-  { icon: <Public />, label: "Hosting" },
+const data: ListBarData[] = [
+  {
+    icon: <DashboardIcon />,
+    label: "Dashboard",
+    link: "home",
+    children: [
+      { icon: <Dns />, label: "Database", link: "home" },
+      { icon: <Public />, label: "Hosting", link: "test" },
+    ],
+  },
+  {
+    icon: <ShoppingCartIcon />,
+    label: "Customers",
+    link: "test",
+    children: [
+      { icon: <Dns />, label: "Database", link: "home" },
+      { icon: <Public />, label: "Hosting", link: "test" },
+    ],
+  },
+  { icon: <People />, label: "Authentication", link: "/dashboard/test/hello" },
+  { icon: <Dns />, label: "Database", link: "home" },
+  { icon: <PermMedia />, label: "Storage", link: "home" },
+  { icon: <Public />, label: "Hosting", link: "home" },
 ];
 
+export default function NestedList({ data }: { data: ListBarData[] }) {
+  const [open, setOpen] = React.useState(new Array(data.length).fill(false));
+
+  function handleClick(index: number) {
+    let list = [...open];
+    list[index] = !list[index];
+    setOpen(list);
+  }
+
+  return (
+    <List
+      sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+      component="nav"
+    >
+      {data.map((item: ListBarData, index: number) => {
+        return item.children ? (
+          <React.Fragment>
+            <ListItemButton
+              component={Rlink}
+              to={item.link}
+              onClick={() => {
+                handleClick(index);
+              }}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.label} />
+              {open[index] ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            {item.children.map((item_c) => {
+              return (
+                <Collapse in={open[index]} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    <ListItemButton
+                      sx={{ pl: 4 }}
+                      component={Rlink}
+                      to={item_c.link}
+                    >
+                      <ListItemIcon>{item_c.icon}</ListItemIcon>
+                      <ListItemText primary={item_c.label} />
+                    </ListItemButton>
+                  </List>
+                </Collapse>
+              );
+            })}
+          </React.Fragment>
+        ) : (
+          <ListItemButton component={Rlink} to={item.link}>
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText primary={item.label} />
+          </ListItemButton>
+        );
+      })}
+      <Divider sx={{ my: 1, alignItems: "center" }} />
+      <UserAvatar userStatus={false}>
+        <UserMenu />
+      </UserAvatar>
+    </List>
+  );
+}
 export function ListBar() {
   return (
     <React.Fragment>
-      <List component="nav">
-        {mainListItems}
-        <Divider sx={{ my: 1, alignItems: "center" }} />
-        <UserAvatar userStatus={false}>
-          <UserMenu />
-        </UserAvatar>
-      </List>
+      <NestedList data={data} />
     </React.Fragment>
   );
 }
