d5e96e7eae66c93d12a4888c174df4b11ed99d19
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2023-01-26 20:37:03 +0800

    引入对话框注销

--- src/pages/dashboard/UserMenu.tsx
+++ src/pages/dashboard/UserMenu.tsx
@@ -4,33 +4,82 @@ import { logout } from "../../api/auth";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export default function UserMenu(): JSX.Element {
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const Logout = () => {
+    logout()
+      .then((res) => {
+        delete_token();
+        setTimeout(() => {
+          navigate("/login");
+        }, 1000);
+      })
+      .catch((err) => {
+        delete_token();
+        setTimeout(() => {
+          navigate("/login");
+        }, 1000);
+      });
+  };
   return (
     <React.Fragment>
       <Button key="one">配置</Button>
       <Button
         fullWidth
         variant="contained"
-        onClick={() => {
-          logout()
-            .then((res) => {
-              delete_token();
-              setTimeout(() => {
-                navigate("/login");
-              }, 1000);
-            })
-            .catch((err) => {
-              delete_token();
-              setTimeout(() => {
-                navigate("/login");
-              }, 1000);
-            });
+        onClick={(event) => {
+          handleClickOpen(event);
         }}
       >
         {t("logout")}
       </Button>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={Logout}>Agree</Button>
+        </DialogActions>
+      </Dialog>
     </React.Fragment>
   );
 }
