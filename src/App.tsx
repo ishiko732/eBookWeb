7e0140d0873a562f32cef989af69b988080ef96d
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2023-01-29 21:55:03 +0800

    优化代码

--- src/App.tsx
+++ src/App.tsx
@@ -143,6 +143,7 @@ const App = () => {
             .catch((err) => {
               if (health === true) {
                 setHealth(false);
+                onHealth.current=false
               }
             });
         }
