b8a9a687df82bb0d34e97f2bdad5f840667277a7
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2023-01-30 20:23:04 +0800

    用户信息卡片化

--- src/pages/dashboard/ConfigBar.tsx
+++ src/pages/dashboard/ConfigBar.tsx
@@ -11,7 +11,7 @@ import Typography from "@mui/material/Typography";
 import { useTranslation } from "react-i18next";
 import { Box } from "@mui/system";
 import SelectLanguage from "../../components/Language";
-import { Chip } from "@mui/material";
+import { Card, CardContent, Chip } from "@mui/material";
 import { userStatus } from "../../api/entity/auth";
 
 const statusColor = (active: userStatus) => {
@@ -41,24 +41,33 @@ const ConfigBar = (props: any) => {
       <Typography variant="h6" gutterBottom align="center">
         {t("config.config.message")}
       </Typography>
-      <Box sx={{ margin: "8px" }}>
-        <Typography gutterBottom>
-          {t("config.config.user", { uid: user.id, name: user.name })}
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          {t("config.config.role", { role: t(`role.${user.role}`) })}
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          {t("config.config.status")}
-          <Chip
-            sx={{ margin: "8px" }}
-            label={t(`userStatus.${user.active}`)}
-            color={statusColor(user.active) as any}
-          />
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          {t("config.config.phone", { phone: user.phone })}
-        </Typography>
+      <Box
+        sx={{ margin: "8px" }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Card sx={{ minWidth: "250px" }} variant="outlined">
+          <CardContent>
+            <Typography gutterBottom>
+              {t("config.config.user", { uid: user.id, name: user.name })}
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+              {t("config.config.role", { role: t(`role.${user.role}`) })}
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+              {t("config.config.status")}
+              <Chip
+                sx={{ margin: "8px" }}
+                label={t(`userStatus.${user.active}`)}
+                color={statusColor(user.active) as any}
+              />
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+              {t("config.config.phone", { phone: user.phone })}
+            </Typography>
+          </CardContent>
+        </Card>
       </Box>
       <Divider variant="middle" />
       <SelectLanguage sx={{ "margin-top": "16px" }} />
