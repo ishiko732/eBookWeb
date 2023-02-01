9c6b76c84d80d6f705bb8b59552fe6279ef2e9f7
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2023-02-01 19:16:08 +0800

    拆分在线列表组件

--- src/pages/system/OnlineControl.tsx
+++ src/pages/system/OnlineControl.tsx
@@ -21,6 +21,7 @@ import { userStatusColor } from "../../config/config";
 import request from "../../config/request";
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
 import { useSnackbar } from "notistack";
+import OnlineDataTable from "./OnlineDataTable";
 
 interface onlineUsers {
   cnt: number | string;
@@ -73,7 +74,6 @@ const OnlineControl = (props: any) => {
     href: string
   ) => {
     e.stopPropagation();
-
     request
       .post(href)
       .then((res) => {
@@ -93,64 +93,7 @@ const OnlineControl = (props: any) => {
         });
       });
   };
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "id", width: 90, sortable: true },
-    {
-      field: "name",
-      headerName: t("auth.register.name") as string,
-      width: 120,
-    },
-    {
-      field: "phone",
-      headerName: t("auth.register.phone") as string,
-      width: 150,
-      sortable: false,
-    },
-    {
-      field: "role",
-      headerName: t("config.config.role").replace(":{{role}}", "") as string,
-      renderCell(params: GridRenderCellParams<any, any, any>) {
-        return t(`role.${params.value || role.TEMP}`);
-      },
-    },
-    {
-      field: "active",
-      headerName: (t("config.config.status") as string).replace(":", ""),
-      renderCell(params: GridRenderCellParams<any, any, any>) {
-        return (
-          <Chip
-            label={t(`userStatus.${params.value || userStatus.EXPIRED}`)}
-            color={userStatusColor(params.value || userStatus.EXPIRED) as any}
-          />
-        );
-      },
-    },
-    {
-      field: "links",
-      headerName: t("system.online.op") as string,
-      renderCell(params: GridRenderCellParams<any, any, any>) {
-        return (
-          <Stack direction="row" spacing={2}>
-            {params.value?.map((v: any) => {
-              return (
-                <Button
-                  variant="contained"
-                  color="error"
-                  onClick={(e) => {
-                    ClickOp(e, params.row, v.href);
-                  }}
-                >
-                  {t(`system.online.links.${v.rel}`)}
-                </Button>
-              );
-            })}
-          </Stack>
-        );
-      },
-      width: 200,
-      sortable: false,
-    },
-  ];
+  
   return (
     <RequiredRole
       user={props.user}
@@ -186,23 +129,7 @@ const OnlineControl = (props: any) => {
             </Tooltip>
           </Stack>
           <Divider light flexItem sx={{ margin: "8px" }} />
-          <Box sx={{ height: 400, width: "100%" }}>
-            <DataGrid
-              // autoHeight
-              rows={message.users}
-              columns={columns}
-              pageSize={5}
-              rowsPerPageOptions={[5]}
-              checkboxSelection
-              disableSelectionOnClick
-              localeText={localeTextConstants(localStorage.language)}
-              initialState={{
-                sorting: {
-                  sortModel: [{ field: "id", sort: "asc" }],
-                },
-              }}
-            />
-          </Box>
+          <OnlineDataTable ClickOp={ClickOp} message={message}/>
         </Paper>
       </Box>
     </RequiredRole>
