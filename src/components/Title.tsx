09014151b4d6e7e3917c3debd1af8db929bcc5e3
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2023-02-18 21:40:55 +0800

    组件修改,支持sx

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
+import { Theme } from "@emotion/react";
+import { SxProps } from "@mui/material";
 
 interface TitleProps {
   children?: React.ReactNode;
+  sx?: SxProps<Theme>;
 }
 
 export default function Title(props: TitleProps) {
   return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+    <Typography
+      component="h2"
+      variant="h6"
+      color="primary"
+      gutterBottom
+      sx={props.sx}
+    >
       {props.children}
     </Typography>
   );
