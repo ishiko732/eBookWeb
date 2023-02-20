0bc507fa94768085cabf99b60b06a6744f42204d
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2023-02-20 22:10:24 +0800

    优化sleep

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,6 +1,11 @@
 export const timer = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
-export async function task() {
-  await timer(1000);
-  console.log("延迟1s");
+export async function task(cnt?: number) {
+  if (!cnt) {
+    cnt = 1;
+  }
+  for (let i = 0; i < cnt; i++) {
+    await timer(1000);
+    console.log("延迟1s");
+  }
 }
