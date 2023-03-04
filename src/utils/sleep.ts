5ac1500d8ba91b0c7cac11ddc7efc5db3f93fcaa
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2023-03-04 21:31:26 +0800

    添加延迟获取数据

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,5 +1,16 @@
 export const timer = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
+export const fetchMockData: (data: any, ms: number) => Promise<any> = (
+  data: any,
+  ms: number
+) => {
+  return new Promise((resolve, _reject) => {
+    setTimeout(() => {
+      resolve(data);
+    }, ms);
+  });
+};
+
 export async function task(cnt?: number) {
   if (!cnt) {
     cnt = 1;
